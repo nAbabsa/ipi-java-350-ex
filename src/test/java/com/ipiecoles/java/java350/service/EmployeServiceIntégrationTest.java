fa1a53da542cbd700c492e79796611a48ff1ef9c
nAbabsa <72446395+nAbabsa@users.noreply.github.com>
2021-02-22 21:51:19 +0100

    Correction Switch

--- src/test/java/com/ipiecoles/java/java350/service/EmployeServiceIntégrationTest.java
+++ src/test/java/com/ipiecoles/java/java350/service/EmployeServiceIntégrationTest.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
-class EmployeServiceIntégrationTest  {
+class EmployeServiceIntegrationTest  {
     @Autowired
     EmployeService employeservice;
     @Autowired
