6cb2760985f502eb945cd8496e9166932bc036ed
nAbabsa <72446395+nAbabsa@users.noreply.github.com>
2021-02-23 17:14:03 +0100

    amélioration coverage

--- src/main/java/com/ipiecoles/java/java350/model/Employe.java
+++ src/main/java/com/ipiecoles/java/java350/model/Employe.java
@@ -226,20 +226,7 @@ public class Employe {
         this.tempsPartiel = tempsPartiel;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Employe)) return false;
-        Employe employe = (Employe) o;
-        return Objects.equals(id, employe.id) &&
-                Objects.equals(nom, employe.nom) &&
-                Objects.equals(prenom, employe.prenom) &&
-                Objects.equals(matricule, employe.matricule) &&
-                Objects.equals(dateEmbauche, employe.dateEmbauche) &&
-                Objects.equals(salaire, employe.salaire) &&
-                Objects.equals(performance, employe.performance);
-    }
-
+    
     @Override
     public int hashCode() {
         return Objects.hash(id, nom, prenom, matricule, dateEmbauche, salaire, performance);
