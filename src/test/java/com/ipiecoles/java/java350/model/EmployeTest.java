7fdcf3a6015391be0e4f10543a9f6cbe0cdc212c
nAbabsa <72446395+nAbabsa@users.noreply.github.com>
2021-02-23 19:04:08 +0100

    Amélioration Coverage

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -41,7 +41,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
     }
      @Test
-     public void getNombreAnneeAncienneteNow(){
+      void getNombreAnneeAncienneteNow(){
          //Given
          Employe e = new Employe();
          e.setDateEmbauche(LocalDate.now());
@@ -50,11 +50,11 @@ import org.junit.jupiter.params.provider.CsvSource;
          Integer anneeAnciennete = e.getNombreAnneeAnciennete();
 
          //Then
-         Assertions.assertThat( anneeAnciennete.intValue()).isEqualTo(0);
+         Assertions.assertThat( anneeAnciennete.intValue()).isZero();
      }
 
      @Test
-     public void getNombreAnneeAncienneteNminus2(){
+      void getNombreAnneeAncienneteNminus2(){
          //Given
          Employe e = new Employe();
          e.setDateEmbauche(LocalDate.now().minusYears(2L));
@@ -67,7 +67,7 @@ import org.junit.jupiter.params.provider.CsvSource;
      }
 
      @Test
-     public void getNombreAnneeAncienneteNull(){
+      void getNombreAnneeAncienneteNull(){
          //Given
          Employe e = new Employe();
          e.setDateEmbauche(null);
@@ -80,7 +80,7 @@ import org.junit.jupiter.params.provider.CsvSource;
      }
 
      @Test
-     public void getNombreAnneeAncienneteNplus2(){
+      void getNombreAnneeAncienneteNplus2(){
          //Given
          Employe e = new Employe();
          e.setDateEmbauche(LocalDate.now().plusYears(2L));
@@ -104,7 +104,7 @@ import org.junit.jupiter.params.provider.CsvSource;
              "2, 'M12345', 0, 1.0, 1700.0",
              "2, 'M12345', 8, 1.0, 2500.0"
      })
-     public void getPrimeAnnuelle(Integer performance, String matricule, Long nbYearsAnciennete, Double tempsPartiel, Double primeAnnuelle){
+      void getPrimeAnnuelle(Integer performance, String matricule, Long nbYearsAnciennete, Double tempsPartiel, Double primeAnnuelle){
          //Given
          Employe employe = new Employe("Doe", "John", matricule, LocalDate.now().minusYears(nbYearsAnciennete), Entreprise.SALAIRE_BASE, performance, tempsPartiel);
 
